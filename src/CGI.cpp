e56df2b3025e801aae4cbeca5012abaeab42b180
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2026-01-12 14:20:12 +0000

    Adde getEnvAsArray() and started CGI execute (pipes for input and output). execute() is not finished

--- src/CGI.cpp
+++ src/CGI.cpp
@@ -38,15 +38,37 @@ void CGI::initializeEnv(const Request &request)
     _env["SCRIPT_FILENAME"] = _scriptPath;
     _env["PATH_INFO"] = request.getPathTarget();
     _env["SERVER_PROTOCOL"] = "HTTP/1.1";
+    // ADD MORE VARS HERE
 }
 
 char ** CGI::getEnvAsArray() const
 {
-    // TO DO
+    char **env = new char*[_env.size() + 1];
+    int i = 0;
+    for (std::map<std::string, std::string>::const_iterator; i != _env.end(), ++it)
+    {
+        std::string element = it->first + "=" + it->second;
+        env[i] = new char[element.size() + 1];
+        strcpy(env[i], element.c_str());
+        i++;
+    } 
+    env[i] == NULL;
+    return (env);
 }
 
 std::string CGI::execute(const Request& request)
 {
     initializeEnv(request);
+    
+    int pipe_in[2];     // Server -> CGI (stdin)
+    int pipe_out[2];    // CGI -> Server (stdout)
+
+    if (pipe(pipe_in) == -1 || pipe(pipe_out) == -1)
+        throw std::runtime_error("Pipe failed on CGI");
+
+    // now we have to fork the process
+
+    
+    
     // TO DO
 }
