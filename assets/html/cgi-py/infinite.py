ed394ee1d61ef7ad5cd9e0ad1add1e2f221095cb
rda-cunh <ricard.mendes@gmail.com>
2026-01-16 00:37:57 +0000

    Addes an infinite loop test in CGI and added teh test into index

---
+++ assets/html/cgi-py/infinite.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import time
+
+# Send headers immediately so the server knows it's alive (optional test case)
+# print("Content-Type: text/html\r\n\r\n") 
+
+while True:
+    time.sleep(1)
